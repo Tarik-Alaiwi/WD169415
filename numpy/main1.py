c49c93ef1b370688837f29e9f8fefd93d98c896a
Tarik-Alaiwi <54720296+Tarik-Alaiwi@users.noreply.github.com>
2023-04-12 12:54:52 +0200

    Add files via upload

---
+++ numpy/main1.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# #ZADANIE 1
+# a = np.arange(0,80,4)
+# print(a)
+#
+# #ZADANIE 2
+# a = np.arange(10, dtype='float')
+# print(a)
+# print(a.dtype)
+# b= a.astype('int32')
+# print(b)
+# print(b.dtype)
+
+# #ZADANIE 3
+# def f(n):
+#     tab = np.array([2**i for i in range(n**2)])
+#     return tab.reshape((n,n))
+# n = 5
+# print(f(n))
