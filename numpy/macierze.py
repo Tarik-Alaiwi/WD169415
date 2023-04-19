52a97a130c930b5e7f679ef02908a4f106265e51
Tarik-Alaiwi <54720296+Tarik-Alaiwi@users.noreply.github.com>
2023-04-19 12:22:28 +0200

    Add files via upload

---
+++ numpy/macierze.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+# a = np.array([[2, 1, 3], [-1, 2, 4]])
+# b = np.array([[1, 5], [2, -2], [-1, 4]])
+#
+# c = np.dot(a, b)
+# print(c)
+# d = a.dot(b)
+# print(d)
+
+# a = np.arange(6).reshape((3, 2))
+# print(a)
+#
+# for b in a:
+#     print(b)
+# print()
+# #drugi sposob
+# for c in range(0, a.shape[0], 1):
+#     print(a[c])
+#     #kazdy elem oddzielnie
+# for d in a.flat:
+#     print(d)
+
+# a = np.arange(6)
+# print(a)
+# print(a.shape)
+#
+# b = a.reshape((2, 3))
+# print(b)
+# print(b.shape)
+#
+# c = a.reshape((3, 2))
+# print(c)
+# print(c.shape)
+#
+# d = a.reshape((6, 1))
+# print(d)
+# print(d.shape)
+#
+# e = c.ravel()
+# print(e)
+#
+# f = c.T
+# print(f)
+# print(f.shape)
+
+# a = np.array([0, 1, 2])
+# b = np.array([0, 1, 2])
+#
+# c = np.dot(a, b)
+# print(c)
+# d = a*b
+# print(d)
